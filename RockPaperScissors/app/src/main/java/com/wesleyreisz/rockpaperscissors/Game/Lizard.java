8d5e947b392bd12c2b398250ff4317ec64e76caa
jfaucette <javonifaucette@hotmail.com>
2015-09-28 16:15:55 -0400

    Pushing project to GitHub
    Pushing the rock paper scissors project to github.

---
+++ RockPaperScissors/app/src/main/java/com/wesleyreisz/rockpaperscissors/Game/Lizard.java
@@ -0,0 +1,25 @@
+package com.wesleyreisz.rockpaperscissors.Game;
+
+import com.wesleyreisz.rockpaperscissors.GameUtils;
+import com.wesleyreisz.rockpaperscissors.R;
+
+/**
+ * Created by JFaucette on 9/28/15.
+ */
+public class Lizard implements GameType {
+
+    @Override
+    public String eval(Integer opponentChoice) {
+        if (opponentChoice== R.id.btnPaper){
+            return GameUtils.BEATS;
+        }else if (opponentChoice==R.id.btnSpock){
+            return GameUtils.BEATS;
+        }else if (opponentChoice==R.id.btnRock){
+            return GameUtils.LOSES_TO;
+        }else if (opponentChoice==R.id.btnScissors){
+            return GameUtils.LOSES_TO;
+        }else {
+            return GameUtils.TIES;
+        }
+    }
+}
